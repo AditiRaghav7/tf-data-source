a9853e8b34370807611b97379d19bd863de4659f
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-03-03 14:57:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,17 @@ output "aws_ami" {
   value = data.aws_ami.name.id
 }
 
+data "aws_subnet" "name" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.name.id]
+  }
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+
 resource "aws_instance" "VPC-ec2" {
   ami                         = "ami-0b02608ac063c1939"
   instance_type               = "t3.nano"
