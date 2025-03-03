4c9542a054a09619cc6527c4d75a4c0d09d4f655
Aditi Raghav <165430462+AditiRaghav7@users.noreply.github.com>
2025-03-03 12:56:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+data "aws_ami" "name" {
+  most_recent = true
+  owners      = ["amazon"]
+}
+
+output "aws_ami" {
+  value = data.aws_ami.name.id
+}
+
 resource "aws_instance" "VPC-ec2" {
   ami                         = "ami-0b02608ac063c1939"
   instance_type               = "t3.nano"
